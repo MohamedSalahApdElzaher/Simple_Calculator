0862ff1b127577243dfd2dc75a5bf58290780494
mohamed-el-sharkawy <47748922+mohamed-el-sharkawy@users.noreply.github.com>
2021-01-05 14:19:52 +0200

    Update simpleCalculator.asm

--- simpleCalculator.asm
+++ simpleCalculator.asm
@@ -25,6 +25,9 @@ operator_msg        BYTE "Enter a valid opertor : ",0               ;message for
 overflow_msg        BYTE "Overflow occures try again ",0   			; message for overflow
 zeroDiv_msg         BYTE "Division by zero is not valid, try again ... ",0         	; message for divide by zero 
 
+overflow1 	    BYTE "overflow occurs in operand1, please enter smaller number", 0
+overflow1   	    BYTE "overflow occurs in operand2, please enter smaller number", 0
+		
 
 addition				BYTE '+', 0
 subtraction				BYTE '-', 0
@@ -53,7 +56,7 @@ main PROC
 		call	WriteString		; write the prompt1 guidance message
 		call	ReadInt			; read 32-bit integer from the user and store it in EAX
 		mov	operand1, eax	        ; copy EAX value to the first operand
-
+		jo overflow1			; jump to overflow2 section if there is overflow in operand1
 	; Ask and get the arithmatic operator
 	get_operator:
 		lea	edx, prompt3
@@ -67,7 +70,8 @@ main PROC
 		lea	edx, prompt2
 		call	WriteString		; write the prompt2 guidance message
 		call	ReadInt			; read 32-bit signed decimal integer from the user and store it in EAX
-		mov	operand2, eax	    ; copy EAX value to the second operand
+		mov	operand2, eax	    	; copy EAX value to the second operand
+		jo overflow2			; jump to overflow2 section if there is overflow in operand2
 
 
 	
@@ -158,6 +162,18 @@ main PROC
 		
     overflow:
 		mov edx , offset overflow_msg    ; print message if overflow found
+		call WriteString
+		jmp quit
+
+	
+	overflow1:		              ; if overflow occurs in operand 1
+		mov edx , offset overflow1    ; ask the user to enter smaller number
+		call WriteString			 
+		jmp quit
+
+		
+	overflow2:			      ; if overflow occurs in operand 1
+		mov edx , offset overflow1    ; ask the user to enter smaller number
 		call WriteString
 		jmp quit
 
