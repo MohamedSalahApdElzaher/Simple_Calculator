27d116a49c81017048a22932d2915da5fcb61421
Mohamed-Abd-El-Nasser <47921242+Mohamed-Abd-El-Nasser@users.noreply.github.com>
2021-01-11 00:54:11 +0200

    Update floatCalculator.asm

--- floatCalculator.asm
+++ floatCalculator.asm
@@ -15,32 +15,18 @@ operator			DB  ?
 prompt1				DB "Enter the first number: ", 0
 prompt2				DB "Enter the second number: ", 0
 prompt3				DB "Choose an operation (+, -, *, /): ", 0
-resultPrompt		DB "Evaluation result: ", 0
+resultPrompt			DB "Evaluation result: ", 0
 
-operator_msg        DB " < invalid opertor, try again ... > ", 0               			; invalid operator exception message
-overflow_msg0       DB " < Result is too large to fit, try again > ", 0   						; overflow exception message
+operator_msg       		DB " < invalid opertor, try again ... > ", 0               			; invalid operator exception message
+overflow_msg0       		DB " < Result is too large to fit, try again > ", 0   				; overflow exception message
 
-equal_sign				DB '=', 0
-space					DB ' ', 0
-
-; for test==============================================================================
-
-;n1 DWORD 5.6
-;n2 DWORD 7.3
-
-;========================================================================================
-
-
+equal_sign			DB '=', 0
+space				DB ' ', 0
 
 .code		; .code is for the executable part of the program
 
 main PROC
-    ;put the two numbers on the stack ====================================================
-        ;fld n1
-        ;fld n2
-    ;=====================================================================================
-
-
+    
 	; Printing the calculator title
 		call	CrLf				; spacing for readability
 		lea	edx, calcTitle		    ; copy the address of caltitle to EDX register
@@ -50,37 +36,36 @@ main PROC
 
     
 	; Ask and get the first number
-	    lea	edx, prompt1		    ; copy the address of caltitle to EDX register
-		call	WriteString			; write the calculator title
-        call    ReadFloat
-		
-        call crlf
+	    lea	edx ,   prompt1		    		; copy the address of caltitle to EDX register
+	    call	WriteString			; write the calculator title
+            call        ReadFloat
+	    call crlf
 	; Ask and get the arithmatic operator
 	get_operator:
 									
 		lea	edx, prompt3
 		call	WriteString				; write the prompt3 guidance message
 		call	ReadChar				; read the operator from the user and store it in AL
-		mov	operator, al	    		; copy the character from AL to operator variable
+		mov	operator, al	    			; copy the character from AL to operator variable
 
-		;jmp check_operator_validity		; check if the operator is a valid 
+		; check if the operator is a valid 
 		cmp operator , '+'                  
 		je n2                     ; jump if equal to do_addition 
 		cmp operator , '-'                  
-		je n2                  ; jump if equal to do_subtraction
+		je n2                  	  ; jump if equal to do_subtraction
 		cmp operator , '*'                   
-		je n2               ; jump if equal to do_multiplication 
+		je n2                     ; jump if equal to do_multiplication 
 		cmp operator , '/'                 
 		je n2                     ; jump if equal to do_division
-       		 jmp invalid_operator
+       		jmp invalid_operator
 
 	; Ask and get the second  number
 		n2:	call crlf
 			call crlf
-			lea	    edx, prompt2		    ; copy the address of caltitle to EDX register
+			lea	edx, prompt2		   	; copy the address of caltitle to EDX register
 			call	WriteString			; write the calculator title
 			call    ReadFloat
-			call crlf
+			call 	crlf
 		
 	; Redirection to the  the needed operator
 
@@ -96,42 +81,42 @@ main PROC
 	; addition opertion 
 
 	do_addition:
-        faddp st(1),st(0)
-		jo overflowBlock0              ; jump if found overflow 
+        	faddp st(1),st(0)
+		jo overflowBlock0             ; jump if found overflow 
 		jmp print_results            ; print resultes
 
 	; subtraction opertion 
 
 	do_subtraction:
 		fsubp st(1),st(0)
-        jo overflowBlock0                  ; jump to overflow section if overflow found
+        jo overflowBlock0                    ; jump to overflow section if overflow found
 		jmp print_results            ; else jump to print_result section
 	
 	; multiplication operation	
 
     do_multiplication:
 		fmulp st(1),st(0)
-        jo overflowBlock0                                ; jump if overflow found
+       		jo overflowBlock0                          ; jump if overflow found
 		jmp print_results                          ; jump to print_results
 					
 
 	do_division: 
 		fdivp st(1),st(0)
-		jo overflowBlock0             ; jump if found overflow 
+		jo overflowBlock0                           ; jump if found overflow 
 		jmp print_results
 
 
 	; handling exceptions 
-	overflowBlock0:		              	; if overflow occurs in results
+	overflowBlock0:		              		    ; if overflow occurs in results
 		call Crlf
 		mov edx , offset overflow_msg0    	
 		call WriteString
 		call Crlf			 
-		jmp quit	      				; exit the program 
+		jmp quit	      			     ; exit the program 
 
 
 	invalid_operator:
-	        call crlf
+	       		call crlf
 			mov edx , offset operator_msg    	
 			call WriteString
 			call Crlf			 
@@ -154,9 +139,9 @@ main PROC
 
 		;Print out the result
         
-        call WriteFloat
-        call	CrLf
-		jmp 	quit
+     		 call WriteFloat
+        	 call	CrLf
+		 jmp 	quit
 
 
 	quit:
